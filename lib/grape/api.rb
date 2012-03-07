e3dbcce4db9c1b2bcc28b04c56cd526412af81e6
Scott Klein <scootklein@gmail.com>
2012-03-07 11:40:32 -0500

    add support for patch call

--- lib/grape/api.rb
+++ lib/grape/api.rb
@@ -282,6 +282,7 @@ module Grape
       def get(paths = ['/'], options = {}, &block); route('GET', paths, options, &block) end
       def post(paths = ['/'], options = {}, &block); route('POST', paths, options, &block) end
       def put(paths = ['/'], options = {}, &block); route('PUT', paths, options, &block) end
+      def patch(paths = ['/'], options = {}, &block); route('PATCH', paths, options, &block) end
       def head(paths = ['/'], options = {}, &block); route('HEAD', paths, options, &block) end
       def delete(paths = ['/'], options = {}, &block); route('DELETE', paths, options, &block) end
       def options(paths = ['/'], options = {}, &block); route('OPTIONS', paths, options, &block) end
